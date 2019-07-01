e48fe9488071a81b9dcd6316769b1a4cf2fa8886
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-07-01 23:42:40 +0430

    add Kanoon.Data

---
+++ Kanoon.Data/Context/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Kanoon.Utility;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kanoon.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public ServiceResult Save()
+        {
+            if (this.SaveChanges() > 0) return ServiceResult.Okay();
+            return ServiceResult.Error();
+        }
+    }
+}
---
+++ Kanoon.Data/Repository.cs
@@ -0,0 +1,19 @@
+﻿using Kanoon.Data.Context;
+using Kanoon.DomainModels.Entities;
+using Kanoon.Utility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kanoon.Data
+{
+    public class Repository
+    {
+        private readonly AppDbContext _context;
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+    }
+}
