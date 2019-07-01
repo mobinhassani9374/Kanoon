0cc62a7622386c687d714e346a7210d81a145ffb
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-07-01 23:34:52 +0430

    add Kanoon.Utility

---
+++ Kanoon.Utility/ServiceResult.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kanoon.Utility
+{
+    public class ServiceResult
+    {
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+
+        public static ServiceResult Okay()
+        {
+            return new ServiceResult()
+            {
+                IsSuccess = true,
+                Message = "عملیات با موفقیت صورت گرفت"
+            };
+        }
+        public static ServiceResult Error()
+        {
+            return new ServiceResult()
+            {
+                IsSuccess = true,
+                Message = "در انجام عملیات خطایی رخ داد مجددا تلاش کنید"
+            };
+        }
+        public static ServiceResult Okay(string message)
+        {
+            return new ServiceResult()
+            {
+                IsSuccess = true,
+                Message = message
+            };
+        }
+        public static ServiceResult Error(string message)
+        {
+            return new ServiceResult()
+            {
+                IsSuccess = true,
+                Message = message
+            };
+        }
+    }
+}
