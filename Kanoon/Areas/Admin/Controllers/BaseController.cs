74c0ee66b9a0f3eb347b7cb3d2ec50e71c362154
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-07-03 22:11:24 +0430

    add BaseController for admin

---
+++ Kanoon/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kanoon.DomainModels.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kanoon.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BaseController<TEntity> : Controller where TEntity : BaseEntity
+    {
+
+    }
+}
