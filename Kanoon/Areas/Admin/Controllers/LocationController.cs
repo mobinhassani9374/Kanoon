db782bd6ee2496a33b140bf590f1d15694c03839
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-07-03 21:58:57 +0430

    develop of  LocationController

--- Kanoon/Areas/Admin/Controllers/LocationController.cs
+++ Kanoon/Areas/Admin/Controllers/LocationController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Kanoon.Data;
+using Kanoon.DomainModels.Entities;
 using Kanoon.DomainModels.Models.Location;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,6 +12,11 @@ namespace Kanoon.Areas.Admin.Controllers
     [Area("Admin")]
     public class LocationController : Controller
     {
+        private readonly BaseRepository<Location> _repo;
+        public LocationController(BaseRepository<Location> repo)
+        {
+            _repo = repo;
+        }
         public IActionResult Index()
         {
             return View();
@@ -18,6 +25,7 @@ namespace Kanoon.Areas.Admin.Controllers
         #region Api
         public IActionResult ApiCreate(LocationCreateModel model)
         {
+
             return Ok();
         }
         #endregion
