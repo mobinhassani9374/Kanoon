55c1bd77c150bc7c939464f87ac36e03397866a7
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-07-03 14:18:55 +0430

    add isotop

--- Kanoon/wwwroot/js/script.js
+++ Kanoon/wwwroot/js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
+
+    $('.grid').isotope({
+        itemSelector: '.grid-item',
+        isOriginLeft: false     
+    });
 });
