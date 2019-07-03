79da2a0e8f1da31c374c5ff992841249c999d30a
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-07-03 23:18:34 +0430

    add ValidationController

---
+++ Kanoon/Controllers/ValidationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DNTPersianUtils.Core;
+
+namespace Kanoon.Controllers
+{
+    public class ValidationController : Controller
+    {
+        public IActionResult PhoneNumber(string id)
+        {
+            var isValid = false;
+
+            if (!string.IsNullOrEmpty(id))
+                isValid = id.IsValidIranianMobileNumber();
+
+            return Ok(isValid);
+        }
+    }
+}
