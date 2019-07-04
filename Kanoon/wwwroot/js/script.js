3097fcacc1e8d2404bae2b814e08a51d6ff8483a
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-07-04 18:04:12 +0430

    data binding in location

--- Kanoon/wwwroot/js/script.js
+++ Kanoon/wwwroot/js/script.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
         "onclick": null,
         "fadeIn": 300,
         "fadeOut": 1000,
-        "timeOut": 40000,
+        "timeOut": 5000,
         "extendedTimeOut": 1000,
       }
 });
