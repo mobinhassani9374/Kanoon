87db947b2e7612c3e8be688447cc89eedf30d978
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-07-04 16:26:15 +0430

    fixed bug

--- Kanoon.DomainModels/Models/Location/LocationCreateModel.cs
+++ Kanoon.DomainModels/Models/Location/LocationCreateModel.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+// using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Kanoon.DomainModels.Models.Location
@@ -10,7 +10,7 @@ namespace Kanoon.DomainModels.Models.Location
         /// <summary>
         /// عنوان محله
         /// </summary>
-        [Required(ErrorMessage = "عنوان محله نمی تواند فاقد مقدار باشد")]
+        // [Required(ErrorMessage = "عنوان محله نمی تواند فاقد مقدار باشد")]
         public string Title { get; set; }
         public string ManagerFullName { get; set; }
 
