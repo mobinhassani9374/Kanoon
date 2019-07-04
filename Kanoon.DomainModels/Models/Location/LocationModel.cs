3279efd09a61bbdde51dea651a802048f41d022f
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-07-04 17:20:34 +0430

    add LocationModel

---
+++ Kanoon.DomainModels/Models/Location/LocationModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kanoon.DomainModels.Models.Location
+{
+    public class LocationModel
+    {
+        public string Title { get; set; }
+
+        public string ManagerFullName { get; set; }
+
+        public List<string> ManagerPhoneNumbers { get; set; } = new List<string>();
+
+        public int CountSuccessor { get; set; }
+    }
+}
