dd5890500ed3aacb7f42488b22078dc60a8a784f
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-07-05 15:11:37 +0430

    add LocationUpdateModel

---
+++ Kanoon.DomainModels/Models/Location/LocationUpdateModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kanoon.DomainModels.Models.Location
+{
+    public class LocationUpdateModel
+    {
+       public int Id { get; set; }
+        // [Required(ErrorMessage = "عنوان محله نمی تواند فاقد مقدار باشد")]
+        public string Title { get; set; }
+        public string ManagerFullName { get; set; }
+
+        public List<string> ManagerPhoneNumbers { get; set; } = new List<string>();
+    }
+}
