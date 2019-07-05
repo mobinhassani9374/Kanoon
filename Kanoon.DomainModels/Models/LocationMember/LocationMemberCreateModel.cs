1e5b5f45f77fc196ab211b5efff7ebbf6f4d90cb
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-07-05 15:45:21 +0430

    add LocationMemberCreateModel

---
+++ Kanoon.DomainModels/Models/LocationMember/LocationMemberCreateModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kanoon.DomainModels.Models.LocationMember
+{
+    public class LocationMemberCreateModel
+    {
+        public int LocationId { get; set; }
+
+        public string FullName { get; set; }
+
+        public List<string> PhoneNumbers { get; set; } = new List<string>();
+    }
+}
