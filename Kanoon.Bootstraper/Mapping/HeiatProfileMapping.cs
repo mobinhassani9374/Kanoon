95d6bb63bb83ea9b1cc4180e78cb9f5c87094626
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-07-15 22:02:49 +0430

    add HeiatProfileMapping

---
+++ Kanoon.Bootstraper/Mapping/HeiatProfileMapping.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+
+namespace Kanoon.Bootstraper.Mapping
+{
+    public class HeiatProfileMapping : Profile
+    {
+        public HeiatProfileMapping()
+        {
+
+        }
+    }
+}
