b9e40a8b91e9a156d1dd0ab4a389cf14fa873c39
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-07-15 22:38:37 +0430

    add Paginated

---
+++ Kanoon.Utility/Pagination/Paginated.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kanoon.Utility.Pagination
+{
+    public class Paginated<T>
+    {
+        public List<T> Data = new List<T>();
+
+        public int CurrentPage { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int TotalPages { get; set; }
+    }
+}
