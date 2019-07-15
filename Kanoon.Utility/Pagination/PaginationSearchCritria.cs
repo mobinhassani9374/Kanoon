6db40f474676cdc51dbfce68264b0f7d669e3070
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-07-15 22:25:11 +0430

    add PaginationSearchCritria

---
+++ Kanoon.Utility/Pagination/PaginationSearchCritria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kanoon.Utility.Pagination
+{
+    public class PaginationSearchCritria
+    {
+        public int PageSize { get; set; }
+
+        public int PageNumber { get; set; }
+    }
+}
