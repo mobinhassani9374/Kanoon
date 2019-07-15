38290835261bdabad1e854e919ff88596d40c922
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-07-15 21:56:25 +0430

    add HeiatRepository

---
+++ Kanoon.Data/Repositories/HeiatRepository.cs
@@ -0,0 +1,15 @@
+﻿using Kanoon.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kanoon.Data.Repositories
+{
+    public class HeiatRepository : BaseRepository<DomainModels.Entities.Heiat>
+    {
+        public HeiatRepository(AppDbContext context) : base(context)
+        {
+
+        }
+    }
+}
