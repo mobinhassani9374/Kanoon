e97bd7caae2a4b3d5254d85ca0f19066b3d9cd26
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-07-15 22:31:10 +0430

    add ExtensionMethods

---
+++ Kanoon.Utility/Pagination/ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Kanoon.Utility.Pagination
+{
+    public static class ExtensionMethods
+    {
+        public static IQueryable<T> ToPaginated<T>(this IQueryable<T> query, PaginationSearchCritria searchCritria)
+        {
+            query = query
+                 .Skip((searchCritria.PageNumber - 1) * searchCritria.PageSize)
+                 .Take(searchCritria.PageNumber);
+
+            return query;
+        }
+    }
+}
