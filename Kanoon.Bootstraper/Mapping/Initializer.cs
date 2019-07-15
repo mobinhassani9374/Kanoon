ae74df7dd7f1a2d6db35077073d8312710308db8
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-07-15 22:11:33 +0430

    add Initializer to bootstraper

---
+++ Kanoon.Bootstraper/Mapping/Initializer.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Kanoon.Bootstraper.Mapping
+{
+    public static class Initializer
+    {
+        public static void Initial()
+        {
+            Mapper.Initialize(c =>
+            {
+                c.AddProfiles(typeof(HeiatProfileMapping).GetTypeInfo().Assembly);
+            });
+
+        }
+    }
+}
