0c356628a8c98696909917a5dd19655d971f1bed
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-07-15 22:20:28 +0430

    develop of ApiCreate in HeiatController

--- Kanoon/Areas/Admin/Controllers/HeiatController.cs
+++ Kanoon/Areas/Admin/Controllers/HeiatController.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Kanoon.Data.Repositories;
+using Kanoon.DomainModels.Models.Heiat;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Kanoon.Areas.Admin.Controllers
 {
-    [Area("Admin")]
-    public class HeiatController : Controller
+    public class HeiatController : BaseController
     {
+        private readonly HeiatRepository _heiatRepository;
+        public HeiatController(HeiatRepository heiatRepository)
+        {
+            _heiatRepository = heiatRepository;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult ApiCreate(HeiatModel model)
+        {
+            var result = _heiatRepository.Create(model);
+            return Ok(result);
         }
     }
 }
