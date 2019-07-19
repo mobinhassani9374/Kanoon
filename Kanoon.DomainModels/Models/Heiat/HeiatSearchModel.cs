eca20b76503ac68a74497c0e27a6cd77a1af95a7
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-07-19 16:14:37 +0430

    add HeiatSearchModel

---
+++ Kanoon.DomainModels/Models/Heiat/HeiatSearchModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kanoon.DomainModels.Models.Heiat
+{
+    public class HeiatSearchModel
+    {
+        public int PageNumber { get; set; }
+
+        public int PageSize { get; set; }
+    }
+}
