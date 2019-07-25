e8f36011e720a797600f96e981a89bf310807b3b
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-07-26 01:19:00 +0430

    develop of login

--- Kanoon/Controllers/AccountController.cs
+++ Kanoon/Controllers/AccountController.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Kanoon.Data.Repositories;
 using Kanoon.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Kanoon.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserRepository _userRepository;
+        public AccountController(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
         [Route("login")]
         public IActionResult Login()
         {
@@ -17,9 +26,36 @@ namespace Kanoon.Controllers
 
         [Route("login")]
         [HttpPost]
-        public IActionResult Login(LoginModel model)
+        public async Task<IActionResult> Login(LoginModel model)
         {
-            return View();
+            var user = _userRepository.AsQueryable().FirstOrDefault(c => c.UserName.Equals(model.UserName) && c.Password.Equals(model.Password));
+
+            if (user == null)
+            {
+                ModelState.AddModelError("", "کاربری یافت نشد");
+            }
+            else
+            {
+                var claims = new List<Claim>();
+                claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+                claims.Add(new Claim(ClaimTypes.Name, user.UserName));
+                claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+                claims.Add(new Claim(ClaimTypes.GivenName, user.FullName));
+
+                var claimIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                var claimPrincipal = new ClaimsPrincipal(claimIdentity);
+
+                await HttpContext.SignInAsync(claimPrincipal, new AuthenticationProperties
+                {
+                    IsPersistent = true,
+                    ExpiresUtc = DateTimeOffset.Now.AddDays(2)
+                });
+
+                return RedirectPermanent("/");
+            }
+
+            return View(model);
         }
     }
 }
