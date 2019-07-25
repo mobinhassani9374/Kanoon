859fa66edc71a395075ca2d189c5aff63536f8cf
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-07-25 11:06:56 +0430

    develop of HeiatController

--- Kanoon/Controllers/HeiatController.cs
+++ Kanoon/Controllers/HeiatController.cs
@@ -2,13 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Kanoon.Data.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Kanoon.Controllers
 {
     public class HeiatController : Controller
     {
-        public IActionResult Index()
+        private readonly HeiatRepository _heiatRepository;
+        public HeiatController(HeiatRepository heiatRepository)
+        {
+            _heiatRepository = heiatRepository;
+        }
+
+        [HttpPost]
+        public IActionResult Search()
         {
             return View();
         }
