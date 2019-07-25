c5729110ce09366087d0ddc7a39f1271efd76ce8
mobinhassani9374 <mahdihassani9374@gmail.com>
2019-07-26 00:59:08 +0430

    [Authorize(Roles = "Admin")]  in BaseController of admin

--- Kanoon/Areas/Admin/Controllers/BaseController.cs
+++ Kanoon/Areas/Admin/Controllers/BaseController.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Kanoon.Data;
 using Kanoon.DomainModels.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Kanoon.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class BaseController : Controller
     {
 
